ce2950af3629903d895a8775fdc9ec3968ba26a0
Mina2702 <khangtranofficial2804@gmail.com>
2023-11-23 14:35:41 +0700

    shopping online

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.f7y1ecp.mongodb.net',
+  DB_USER: 'khangtran2804',
+  DB_PASS: 'khang28042003',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'anhba',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'khang123lxag@gmail.com', // gmail service
+  EMAIL_PASS: 'svyp qnky zmld xjlr'
+};
+module.exports = MyConstants;
